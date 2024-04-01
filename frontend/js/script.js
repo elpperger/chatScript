f10231d6a30a1c4f945ba1d4a5c6c52f45a6240b
elpperger <elpperger@gmail.com>
2024-04-01 12:01:43 -0300

    service onrender

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -132,8 +132,8 @@ const handleLogin = (event) => {
     login.style.display = "none";
     chat.style.display = "flex";
 
-    // websocket = new WebSocket("wss://chatscript.onrender.com")
-    websocket = new WebSocket("ws://localhost:8080");
+    websocket = new WebSocket("wss://chatscript.onrender.com")
+    // websocket = new WebSocket("ws://localhost:8080");
     websocket.onmessage = processMessage;
 };
 
